0a0da3b7080eff8b2f7b85ece0e5bfa58f3c61d2
riz2001 <mohammedrizwanps@gmail.com>
2024-06-07 22:36:56 +0530

    completed

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -21,32 +21,14 @@ const ViewAll = () => {
            
                   <tr>
               
+                    <td>THE KING</td>
                     <td>RIZWAN</td>
-                    <td>9142211987</td>
                     <td>SUHAIL</td>
-                    <td>mohammedrizwan@gmail.com</td>
+                    <td>240</td>
                   </tr>
-                  <tr>
-                
-                    <td>JERRIN</td>
-                    <td>7025768231</td>
-                    <td>SREERAG</td>
-                    <td>jerrin@gmail.com</td>
-
-                  </tr>
-                  <tr>
-                    <td>AKASH</td>
-                    <td>7025768231</td>
-                    <td>NEVIN</td>
-                    <td>akash@gmail.com</td>
-                 </tr>
-                 <tr>
-                    <td>ALVIN</td>
-                    <td>7025787678</td>
-                    <td>JOE</td>
-                    <td>alvin@gmail.com</td>
-                 </tr>
                
+                
+                  
                 
               </table>
         </div>
